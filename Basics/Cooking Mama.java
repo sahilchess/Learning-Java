b706b23f298eb5386332bf94765e168f5216c0aa
Sahil Chess <125219792+sahilchess@users.noreply.github.com>
2025-12-29 11:39:07 -0500

    Create Cooking Mama Java program
    Add a simple Java program for cooking mac and cheese.

---
+++ Basics/Cooking Mama.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("get mac from walmart");
+        System.out.println("get cheese"); 
+        System.out.println("boil mac");
+        System.out.println("add chees in boiling mac");
+        System.out.println("stop boiling the mac and cheese")
+        System.out.println("pour mac and cheese onto plate")
+        System.out.println("eat")
+    }
+}
