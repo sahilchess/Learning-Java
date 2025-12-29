8dade7136f8da83deeca3ea2af0f5e521afe8501
Sahil Chess <125219792+sahilchess@users.noreply.github.com>
2025-12-29 13:55:07 -0500

    Add HelloWorld class with room size variables

---
+++ Variables/06 Storage Box.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Write your code here 💖
+        int livingRoom = 15;
+        int masterBedroom = 45;
+        int bathroom = 10;
+        System.out.println(livingRoom);
+        System.out.println(masterBedroom);
+        System.out.println(bathroom);
+
+        
+    }
+}
