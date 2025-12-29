39924e1e4799754031f03c322a7a9588f679b28f
Sahil Chess <125219792+sahilchess@users.noreply.github.com>
2025-12-29 11:45:56 -0500

    Create 04 Comments and Errors.java

---
+++ Basics/04 Comments and Errors.java
@@ -0,0 +1,17 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Write Your code here 💖
+        System.out.println("Getting Launch Codes");
+        System.err.println("Connecting to server…"); 
+        System.err.println("Error: Connection timed out."); 
+        System.err.println("Retrying…"); 
+        System.out.println("Launching Sequence Initiated");
+        System.err.println("Error: Rocket Missing");
+        System.out.println("Typo: Getting Lunch Codes");
+
+            /*
+         System.err.println() is used to print error messages! 😵😵❌
+         It is similar to System.out.println() but is used to print error messages.
+            */
+    }
+}
