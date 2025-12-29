94fabded9c467c3f31b5df37686abbdb661ef675
Sahil Chess <125219792+sahilchess@users.noreply.github.com>
2025-12-29 13:48:03 -0500

    Add Music Wrapped Java program

---
+++ Basics/Music Wrapped.java
@@ -0,0 +1,15 @@
+/**
+ * Woah? What's this? 🤔 It's a weird looking comment. Another *, eh? *  
+ * I'm a comment, and I want to say hi! 👋
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("========== 🎵 Music Wrapped 🎵 ==========");
+        System.out.println("Squabble Up"); // Kendrik
+        System.out.println("Prairies"); // BoyWithUke
+        System.out.println("Die Trying"); // PARTYNEXTDOOR
+        System.out.println("Runaway Baby"); // Bruno Mars
+        System.out.println("======= 🎵 🎵 🎵 🎵 🎵 🎵 🎵 🎵 ========");
+    }
+}
